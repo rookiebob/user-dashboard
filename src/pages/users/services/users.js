eebd21e0b17ab4bf4a6d3174a0bb94cc33eb19b3
rookiebob <rookiebob@163.com>
2018-05-07 19:29:02 +0800

    dev ui && delete

---
+++ src/pages/users/services/users.js
@@ -0,0 +1,12 @@
+import { PAGE_SIZE } from '../constants';
+import request from '../../../utils/request';
+
+export function fetch({ page = 1 }) {
+    return request(`/api/users?_page=${page}&limit=${PAGE_SIZE}`);
+}
+
+export function remove(id) {
+    return request(`/api/users/${id}`, {
+        method: 'DELETE',
+    });
+}
